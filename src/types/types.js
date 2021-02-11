9367f58821e9750ee35c9ad66b05883abd13a710
Alonso <u201421613@upc.edu.pe>
2021-02-11 12:42:27 -0500

    Aplicación de calendario

---
+++ src/types/types.js
@@ -0,0 +1,21 @@
+export const types = {
+    uiOpenModal: '[ui] Open Modal',
+    uiCloseModal: '[ui] Close Modal',
+
+    eventStartNew: '[Event] Start add new',
+    eventAddNew: '[Event] Add new',
+    eventSetActive: '[Event] Set active',
+    eventLogout: '[Event] Logout event',
+    eventClearActiveEvent: '[Event] Clear active event',
+    eventUpdate: '[Event] Event updated',
+    eventDeleted: '[Event] Event deleted',
+    eventLoaded: '[Event] Events loaded',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start registrer',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+}
